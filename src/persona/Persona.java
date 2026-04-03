6ed772986875ce383a8ae025942c430dfcf8de12
Leandro Lambardi <leandrolambardi@hotmail.com>
2026-04-03 03:17:01 -0300

    agregar atributos y getters setters a clase Persona

--- src/persona/Persona.java
+++ src/persona/Persona.java
@@ -1,9 +1,39 @@
 package persona;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+enum Sexo{
+    Masculino,
+    Femenino,
+    Otro
+}
+
 public class Persona {
 
     private String nombre;
     private String apellido;
+    private byte edad;
+    private int id;
+    private int dni;
+    private String profesion;
+    private LocalDate fechaDeNacimiento;
+    private String eMail;
+    private String telefono;
+    private String calle;
+    private String altura;
+    private String localidad;
+    private String pais;
+    private boolean activo;
+    private boolean suscrito;
+    private LocalDateTime fechaDeCreacion;
+    private Sexo sexo;
+
+
+
+
+
+
 
     public Persona(String nombre, String apellido){
         this.nombre = nombre;
@@ -12,6 +42,127 @@ public class Persona {
     @Override
     public String toString(){
         return "Nombre = " + this.nombre + "\n" + "Apellido = " + this.apellido;
+
+    }
+
+    public byte getEdad() {
+        return edad;
+    }
+
+    public void setEdad(byte edad) {
+        this.edad = edad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+
+    public LocalDate getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(LocalDate fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getAltura() {
+        return altura;
+    }
+
+    public void setAltura(String altura) {
+        this.altura = altura;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public boolean isSuscrito() {
+        return suscrito;
+    }
+
+    public void setSuscrito(boolean suscrito) {
+        this.suscrito = suscrito;
+    }
+
+    public LocalDateTime getFechaDeCreacion() {
+        return fechaDeCreacion;
+    }
+
+    public void setFechaDeCreacion(LocalDateTime fechaDeCreacion) {
+        this.fechaDeCreacion = fechaDeCreacion;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
     }
 
     public String getNombre() {
